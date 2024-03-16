967fb49f4cf9e4a797b4f191b5a15b9b6c8b5fd6
Hidocmatn <ql2012hhh@126.com>
2024-03-16 08:46:51 +0800

    在KubeJS中注册配方类型

--- src/main/java/com/tac/guns/compat/kubejs/TimelessKubeJSPlugin.java
+++ src/main/java/com/tac/guns/compat/kubejs/TimelessKubeJSPlugin.java
@@ -1,6 +1,14 @@
 package com.tac.guns.compat.kubejs;
 
+import com.tac.guns.GunMod;
+import com.tac.guns.compat.kubejs.recipe.TimelessGunSmithRecipeJS;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.recipe.RegisterRecipeHandlersEvent;
+import net.minecraft.resources.ResourceLocation;
 
 public class TimelessKubeJSPlugin extends KubeJSPlugin {
+    @Override
+    public void addRecipes(RegisterRecipeHandlersEvent event) {
+        event.register(new ResourceLocation(GunMod.MOD_ID, "gun_smith_table_crafting"), TimelessGunSmithRecipeJS::new);
+    }
 }
